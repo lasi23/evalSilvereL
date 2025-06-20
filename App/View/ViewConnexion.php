ed5b646fbabbeffe1d10ab88238e28b6d73eb595
lasi23 <lsilvere39@gmail.com>
2025-06-20 14:56:24 +0200

    suppression mauvais fichier et rajout du bon fichier avec sont code al'intérieur

---
+++ App/View/ViewConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTOTYPE html>
+<html lang="fr">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="./public/assets/styles/main.css">
+   <script src="./public/assets/scripts/main.js"async></script>
+   <tittle>Connexion</tittle>
+</head>
+<body>
+   <h1>Se connecter</h1>
+   <form action="" method="post">
+      <label for="email">Veuillez saisir votre adresse mail : </label>
+      <input type="email" name"email">
+      <label for="password">Veuillez saisir votrer mot de passe : </label>
+      <input type="password" name="password">
+      <input type="submit" name:"submit" value="connexion">
+   </form>
+</body>
+</html>
